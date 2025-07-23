6c516232ec652fc46b7565708428e178c69e195f
Ganesh Kumbhar (ADMIN) <ganeshkadmin@gorillaexpense.com>
2025-07-23 22:19:44 +0530

    Implement vnet peering and create db-vm.

--- dev/main.tf
+++ dev/main.tf
@@ -9,6 +9,15 @@ module "hub_vnet" {
   subnets = {
     bastion = "10.0.0.0/24"
   }
+  peerings = {
+    spoke-to-hub = {
+      virtual_network_name =  module.spoke_vnet.vnet_name 
+      remote_virtual_network_id = module.spoke_vnet.vnet_id
+      allow_forwarded_traffic   = true
+      allow_gateway_transit     = false
+      use_remote_gateways       = false
+    }
+  }
 }
 
 module "spoke_vnet" {
@@ -21,6 +30,15 @@ module "spoke_vnet" {
   subnets = {
     app = "10.1.1.0/24"
     db  = "10.1.2.0/24"
+  }
+  peerings = {
+    hub-to-spoke = {
+        virtual_network_name =  module.hub_vnet.vnet_name
+      remote_virtual_network_id = module.hub_vnet.vnet_id
+      allow_forwarded_traffic   = true
+      allow_gateway_transit     = false
+      use_remote_gateways       = false
+    }
   }
 }
 
@@ -40,4 +58,47 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   remote_virtual_network_id = module.hub_vnet.vnet_id
   allow_forwarded_traffic   = true
   allow_virtual_network_access = true
+}
+
+
+#VM creation
+resource "azurerm_network_interface" "db_nic" {
+  name                = "db-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.spoke_vnet.subnet_ids["db"]
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "db_vm" {
+  name                = "db-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_D2s_v3"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.db_nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+    tier        = "database"
+  }
 }
--- dev/variables.tf
+++ dev/variables.tf
@@ -19,3 +19,15 @@ variable "address_space" {
 variable "subnets" {
   type = map(string)
 }
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
+
